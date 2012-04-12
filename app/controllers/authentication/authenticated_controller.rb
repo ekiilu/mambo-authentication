cdd2496803cd36dd76f374062a0a77cc36619417
Chris Dion <chris@verticallabs.ca>
2012-04-12 11:26:20 -0700

    add user

--- app/controllers/authentication/authenticated_controller.rb
+++ app/controllers/authentication/authenticated_controller.rb
@@ -4,8 +4,11 @@ module Authentication
 
 		#
 		def requires_authentication
+			logger.debug(authenticated_user)
 			if authenticated_user.nil?
 				session[:back] = request.url
+				logger.debug(session[:back])
+				logger.debug(authentication.new_session_path)
 				redirect_to(authentication.new_session_path)
 			end
 		end
