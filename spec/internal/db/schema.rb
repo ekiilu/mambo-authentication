ad81dc11c84d4712d71a8bef876a5fd9a8a2aef6
Chris Dion <chris@verticallabs.ca>
2012-06-19 12:30:25 -0700

    stuff

--- spec/internal/db/schema.rb
+++ spec/internal/db/schema.rb
@@ -1,3 +1,7 @@
+puts File.join(__FILE__, "..", "..", "..", "db", "migrate", "**", "*.rb")
+
+Dir[File.join(__FILE__, "..", "..", "..", "db", "migrate", "**", "*.rb")].each { |f| require f }
+
 ActiveRecord::Schema.define do
   #
 end
