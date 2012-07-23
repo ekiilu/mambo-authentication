02f73e53aec9cd5784621aee6f056833151a2905
Chris Dion <chris@verticallabs.ca>
2012-07-23 14:56:44 -0700

    fix require and dep warnings

--- lib/authentication.rb
+++ lib/authentication.rb
@@ -1,4 +1,17 @@
 # -*- encoding : utf-8 -*-
+require "rails"
+require "dm-core"
+require "dm-rails"
+require "dm-types"
+require "dm-validations"
+require "dm-constraints"
+require "dm-transactions"
+require "dm-timestamps"
+require "dm-accepts_nested_attributes"
+require "will_paginate"
+require "haml-rails"
+require "mambo-support"
+
 require "authentication/version"
 require "authentication/engine"
 require "authentication/authenticated_user"
