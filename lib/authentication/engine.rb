7d383c054f9531057af23b341501ad89e47c61da
Chris Dion <chris@verticallabs.ca>
2012-08-01 14:23:35 -0700

    arg

--- lib/authentication/engine.rb
+++ lib/authentication/engine.rb
@@ -3,6 +3,12 @@ module Authentication
   class Engine < Rails::Engine
     isolate_namespace Authentication
 
+    initializer "models" do
+      require "authentication/user"
+      require "authentication/role"
+      require "authentication/user_role"
+    end
+
 		#
     initializer "i18n" do
     	I18n.load_path += Dir[File.join(__FILE__, "config", "locales", "**", "*.{rb,yml}")]
--- lib/authentication/version.rb
+++ lib/authentication/version.rb
@@ -1,5 +1,5 @@
 # -*- encoding : utf-8 -*-
 module Authentication
-  VERSION = "0.0.10"
+  VERSION = "0.0.11"
   DM_VERSION = "~> 1.2"
 end
