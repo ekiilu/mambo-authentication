612d054a937004471fbb5c19d58ee1e297b183f0
Chris Dion <chris@verticallabs.ca>
2012-08-01 10:52:21 -0700

    blah

--- lib/authentication.rb
+++ lib/authentication.rb
@@ -12,10 +12,10 @@ require "will_paginate"
 require "haml-rails"
 require "mambo-support"
 
+module Authentication
+end
+
 require "authentication/version"
 require "authentication/engine"
 require "authentication/authenticated_user"
 require "authentication/authenticated_controller"
-
-module Authentication
-end
--- lib/authentication/engine.rb
+++ lib/authentication/engine.rb
@@ -4,13 +4,6 @@ module Authentication
     isolate_namespace Authentication
 
 		#
-    initializer "models" do
-    	require "authentication/user"
-    	require "authentication/credentials"
-    	require "authentication/session"
-		end
-
-		#
     initializer "i18n" do
     	I18n.load_path += Dir[File.join(__FILE__, "config", "locales", "**", "*.{rb,yml}")]
     end
